5a2f9f80f229903a5bfa95209ba9acd2bbc65521
Igor Sadza <isadza@smallboi.home>
2026-05-12 01:42:11 +0200

    ci: fighting with infra

--- platform-products/platform-storage/applications/seaweedfs/test/script.sh
+++ platform-products/platform-storage/applications/seaweedfs/test/script.sh
@@ -13,14 +13,22 @@ dpkg -i 5cmd_2.3.0_linux_amd64.deb
 kubectl port-forward -n seaweedfs svc/seaweedfs-s3 8333:8333 2>&1 &
 
 # -----------------
-# Prepare Credentials File
+# Prepare Credentials File (generated-one)
 # -----------------
-ACCESS_KEY_ID=$(kubectl get secret -n seaweedfs seaweedfs-s3-secret -o jsonpath='{.data.admin_access_key_id}' | base64 --decode)
-SECRET_ACCESS_KEY=$(kubectl get secret -n seaweedfs seaweedfs-s3-secret -o jsonpath='{.data.admin_secret_access_key}' | base64 --decode)
+ACCESS_KEY=$(kubectl get secret -n seaweedfs seaweedfs-s3-secret -o jsonpath='{.data.admin_access_key_id}' | base64 --decode)
+SECRET_KEY=$(kubectl get secret -n seaweedfs seaweedfs-s3-secret -o jsonpath='{.data.admin_secret_access_key}' | base64 --decode)
+
+# -----------------
+# Prepare Credentials File (configuration)
+# -----------------
+#CONFIG=$(kubectl get secret -n seaweedfs seaweedfs-s3-secret -o jsonpath='{.data.seaweedfs_s3_config}' | base64 --decode)
+#ACCESS_KEY=$(echo ${CONFIG} | jq -r '.[] | .[] | .credentials[] | .accessKey')
+#SECRET_KEY=$(echo ${CONFIG} | jq -r '.[] | .[] | .credentials[] | .secretKey')
+
 cat << EOF > credentials
 [default]
-aws_access_key_id = ${ACCESS_KEY_ID} 
-aws_secret_access_key = ${SECRET_ACCESS_KEY} 
+aws_access_key_id = ${ACCESS_KEY} 
+aws_secret_access_key = ${SECRET_KEY} 
 EOF
 
 # -----------------
